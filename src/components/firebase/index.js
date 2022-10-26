9024ceec03840e29405ea7f0c5de3aa58aaae048
tahsinchouhan <tahsinchouhan2000@gmail.com>
2022-10-26 15:06:12 +0530

    mustafa-2

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
-import "firebase/database";
 import "firebase/auth";
+import "firebase/database";
 
 const prodConfig = {
   apiKey: "AIzaSyBcTPvgOcuLoUrFvMJvFTFqMzxhJC5mSNM",
@@ -10,7 +10,7 @@ const prodConfig = {
   storageBucket: "bowoot-be754.appspot.com",
   messagingSenderId: "300482239919",
   appId: "1:300482239919:web:7fa29e50bd389f84",
-  measurementId: "G-X0B2WP80EK"
+  measurementId: "G-X0B2WP80EK",
 };
 
 const devConfig = {
@@ -21,7 +21,7 @@ const devConfig = {
   storageBucket: "bowoot-test.appspot.com",
   messagingSenderId: "109691704068",
   appId: "1:109691704068:web:7e8a35126cff8258a07651",
-  measurementId: "G-RPSTRHS607"
+  measurementId: "G-RPSTRHS607",
 };
 
 const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
